0397e03f36a9864043e7c512480233c795e2dedf
Isac Emanuel <silva.isac@escolar.ifrn.edu.br>
2026-08-24 11:54:39 -0300

    Questao 1

---
+++ Lista1/Q1/Quest_1.ts
@@ -0,0 +1,13 @@
+//Questão 1:
+
+/*
+Calcule o volume de um cilindro dada a sua altura (h) e o seu raio (r) pela fórmula:
+v =  pi x r² x h 
+
+Quadrado: r ** 2 ou r * r
+*/
+
+const pi: number = 3.14;
+const r: number = 5;
+const h: number = 10;
+
